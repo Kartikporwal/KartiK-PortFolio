30ab031f10a450809cbe2fda9934f0d101467c9f
kartik Porwal <porwalkartik14@gmail.com>
2026-03-06 14:25:46 +0530

    upload

--- src/components/Section1/Navbar.jsx
+++ src/components/Section1/Navbar.jsx
@@ -10,9 +10,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
         {/* Logo */}
-        <h1 className="text-2xl font-bold text-pink-500">
-          KARTIK PORWAL
-        </h1>
+        <img className="w-50 h-10"src="logo.png" alt="" />
 
         {/* Desktop Menu */}
         <div className="hidden md:flex gap-10 text-lg font-semibold text-gray-700">
